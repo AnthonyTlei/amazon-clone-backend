19d1d82e00f15c789dc42d62d031e2000d7583ac
Anthony Tleiji <anthonytleiji@gmail.com>
2023-03-20 18:00:15 +0200

    Implemented CRUD operations for product

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Get, Param, Patch, Delete } from '@nestjs/common';
+import { ProductService } from './product.service';
+import { ProductDocument } from './product.schema';
 
 @Controller('product')
-export class ProductController {}
+export class ProductController {
+  constructor(private productService: ProductService) {}
+
+  @Post()
+  create(
+    @Body('name') name: string,
+    @Body('price') price: number,
+    @Body('description') description: string,
+  ): Promise<ProductDocument> {
+    return this.productService.create(name, price, description);
+  }
+
+  @Get()
+  findAll(): Promise<ProductDocument[]> {
+    return this.productService.findAll();
+  }
+
+  @Get(':id')
+  find(@Param('id') id: string): Promise<ProductDocument> {
+    return this.productService.find(id);
+  }
+
+  @Patch(':id')
+  updateProduct(
+    @Param('id') id: string,
+    @Body('name') name: string,
+    @Body('price') price: number,
+    @Body('description') description: string,
+  ): Promise<ProductDocument> {
+    return this.productService.update(id, name, price, description);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.productService.delete(id);
+  }
+
+}
