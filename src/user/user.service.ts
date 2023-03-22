6bdd22693f572c21887e6b46c7aeaf94999fe95d
Anthony Tleiji <anthonytleiji@gmail.com>
2023-03-22 10:00:24 +0200

    Code Setup: Added User Module, Schema, Interface, Service and Controller Added Auth Module, Service and Controller

---
+++ src/user/user.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { UserDocument } from './user.schema';
+import { UserDetails } from './user-details.interface';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectModel('User') private readonly userModel: Model<UserDocument>,
+  ) {}
+
+  _getUserDetails(user: UserDocument): UserDetails {
+    return {
+      id: user._id,
+      name: user.name,
+      email: user.email,
+    };
+  }
+}
