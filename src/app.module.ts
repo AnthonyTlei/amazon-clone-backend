e1838df7c61b3d680b95f5635405ae69dbffa348
Anthony Tleiji <anthonytleiji@gmail.com>
2023-04-03 19:07:55 +0300

    Initial setup for stripe integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,15 +6,17 @@ import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { StripeModule } from './stripe/stripe.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/amazon'),
     ProductModule,
     UserModule,
-    AuthModule
+    AuthModule,
+    StripeModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {}
